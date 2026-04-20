1806c9706ff764afeea44fac6b1b4586d1fc4f69
ZOEY <zoeychu0709@gmail.com>
2026-04-20 20:10:08 +0800

    Fixed security_question

--- password_system/password_security.py
+++ password_system/password_security.py
@@ -7,6 +7,8 @@ from password_hashing import hash_password
 from user_data import save_user
 #Import function to save user data
 
+from security_question import set_security_question
+
 username = input("Enter username: ")
 password = input("Enter password: ")
 
@@ -16,19 +18,22 @@ if not is_valid_password(password):
           "Password must be at least 8 characters long, " \
           "contain uppercase and lowercase letters, "
           "and include numbers.")
+else:
+    hashed = hash_password(password)
 
-    else:
-        hashed = hash_password(password)
-
-        user={
-            "username": username,
-            "password": hashed
+    question, hashed_answer = set_security_question()
+    
+    user={
+        "username": username,
+        "password": hashed,
+        "security_question": question,
+         "security_answer": hashed_answer
         }
         #Create user data
         #key = "username"  # field name
         #value = username  # data
 
-        save_user(user)
+    save_user(user)
         #Save to JSON
-
-        print("User registered successfully!")  
+        
+    print("User registered successfully!")  
