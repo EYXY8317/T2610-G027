20832b7c7a3e610141c3b1f9dfa38f12d4e06c66
ZOEY <zoeychu0709@gmail.com>
2026-04-25 07:06:12 +0800

    Fix crud.py(add)  and app.py

--- Journal_Pages/app.py
+++ Journal_Pages/app.py
@@ -1,34 +1,24 @@
 from flask import Flask, render_template, request
-#Import tools
-#Flask = Create the website
-#Render_template = Show HTML page
-#Request = Get user input
-from create_read import load_entries, add_entry
+#Render_template = Show HTML page (display page + send data) - output
+#Request = Get user input (from form) - input
+from crud import load_entries, add_entry
 
 app = Flask(__name__)
-#Create a Flask application
 
-@app.route("/", methods=["GET", "POST"])
-#app.route = Define the URL for the home page
-# / = Home page
-#Methods = Allow both GET and POST requests
-#GET = Open the page
-#POST = Submit form data
-
+@app.rounte("/", methods=["GET", "POST"])
 def home():
-#Define the function to handle the home page
-    entries = load_entries()
-
-    if request.method == "POST":
-    #User submits a new journal entry
-    #Post = Send data to the server
-        content = request.form["content"]
-         #Get text from form
-        entries = add_entry(content)
-
-    return render_template("journal.html", entries = entries)
     
-if __name__ == "__main__":
-#Check if the script is run directly (not imported as a module)
-    app.run(debug=True)
-    #Start the Flask development server with debug mode enabled
+    if request.method == "POST":
+        content = request.form.get("content")
+
+        if content:
+            add_entry(content)
+
+        
+        entries = load_entries()
+
+        return render_template("journal.html",entries=entries)
+    
+app.run(debug=True)
+
+
