d6d4a733f2462cec56530421a66c37bfc23ab54c
ZOEY <zoeychu0709@gmail.com>
2026-04-25 09:50:37 +0800

    Add delete function (crud.py)

--- Journal_Pages/crud.py
+++ Journal_Pages/crud.py
@@ -18,7 +18,9 @@ def load_entries():
 #Add function
 def add_entry(content):
     entries = load_entries()
-
+    #Entries = List
+    #Entry = List
+    
     new_id = 1
     if entries:
         new_id = max(e["id"] for e in entries) + 1
@@ -37,5 +39,22 @@ def add_entry(content):
 
     with open("json.json","w") as file:
         json.dump(emtries, file, indent=4)
+
+    return entries
+
+def delete_entry(entry_id):
+    entries = load_entries()
+
+    entries = [e for e in entries if str(e["id"]) != str(entry_id)]
+    #Create a new list of entries
+    #For e in entries = loop through each entry
+    #E["id"] = get the id of each entry
+    #Str(e["id"]) != str(entry_id)
+    #Keep the entry if its id is NOT equal to the delete id
+    #把“符合条件的”全部放进新 list
+    
+    with open("journal.json","w") as file:
+    #Open the file and prepare to overwrite with new data
+        json.dump(entries, file, indent=4)
 
     return entries
