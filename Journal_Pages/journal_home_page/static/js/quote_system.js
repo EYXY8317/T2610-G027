f82df758c68ec8ddcd775c270262d8ff71c82724
ZOEY <zoeychu0709@gmail.com>
2026-05-11 00:45:43 +0800

    Add Journal  Quote random function

--- Journal_Pages/journal_home_page/static/js/quote_system.js
+++ Journal_Pages/journal_home_page/static/js/quote_system.js
@@ -6,6 +6,51 @@ const quoteSettingButton =
 const quoteOverlay =
     document.querySelector("#quote-popup-overlay");
 
+const quoteText =
+    document.querySelector("#quote-text");
+
+
+// ================= QUOTE DATA =================
+
+let quotes = {};
+
+
+// ================= LOAD JSON =================
+
+fetch("/homepage_static/json/quotes.json")
+
+.then(function(response) {
+
+    return response.json();
+
+})
+
+.then(function(data) {
+
+    console.log(data);
+
+    quotes = data;
+
+    showRandomQuote("Motivation");
+
+});
+
+
+// ================= RANDOM QUOTE =================
+
+function showRandomQuote(category) {
+
+    const categoryQuotes =
+        quotes[category];
+
+    const randomIndex =
+        Math.floor(Math.random() * categoryQuotes.length);
+
+    quoteText.textContent =
+        categoryQuotes[randomIndex];
+
+}
+
 
 // ================= OPEN POPUP =================
 
