8dbe68ff4043371e71dd73b8021136765fa51679
Liong Xin Ying <yxin683@gmail.com>
2026-06-23 19:09:10 +0800

    feat(theme): update theme color switching function

--- Calendar_Pages/static/task2.js
+++ Calendar_Pages/static/task2.js
@@ -1,39 +1,4 @@
-function setMochaTheme(){
 
-    document.documentElement.style
-    .setProperty(
-        "--primary",
-        "#8b6b4f"
-    );
 
-    document.documentElement.style
-    .setProperty(
-        "--primary-light",
-        "#f6eee7"
-    );
 
-    document.documentElement.style
-    .setProperty(
-        "--primary-hover",
-        "#eadccf"
-    );
 
-    document.documentElement.style
-    .setProperty(
-        "--primary-border",
-        "#e7d8c9"
-    );
-
-    document.documentElement.style
-    .setProperty(
-        "--gradient-start",
-        "#c9a07e"
-    );
-
-    document.documentElement.style
-    .setProperty(
-        "--gradient-end",
-        "#8b6b4f"
-    );
-
-}
